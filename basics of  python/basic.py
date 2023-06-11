5e720bfcaaed9884c87e2981735f3aac732ffdbd
AbraraahamedD <134491672+AbraraahamedD@users.noreply.github.com>
2023-06-11 18:00:31 +0530

    Add files via upload

---
+++ basics of  python/basic.py
@@ -0,0 +1,69 @@
+print("hello world")
+# single line comment 
+# it is only visible in the  code but not in the output
+""" 
+multiple line comment 
+
+"""
+#VARIABLE
+
+number = 5
+number77 = 10
+numberOfBikes = 15
+first_name_of_the_person = 6
+
+thePerson = 7
+abrarAhmed = 3
+beSmartAndUnique = 2
+
+print(number)
+print("number")
+
+bike = numberOfBikes
+model = 2023 
+print(bike , model)
+ 
+x, y, z = 20 , 15 , 25 
+print(x)
+print(y)
+print(z)
+
+x, y, z = "sun" , "moon" , "star"
+print(x, y, z )
+
+number1 = 23   #integer
+number2 = 9.7  #float
+number3 = "abrar"  #string
+
+print(number1)
+print(number2)
+print(number3)
+
+print(type(number2))
+
+demo1 = 14  #integer
+print(float(demo1))
+
+#string 
+text = "abrar"
+print(len (text)) 
+
+text = "abrar ahmed"
+print(len(text))
+
+text = "hello world"
+print(text[5:]) 
+
+
+x = 25  #integer
+y = 40.5 #float
+z = "abrar" #string 
+a = True  #bollean 
+b = False #bollean
+#bollean = True , False  = 0,1 
+print(a)
+print(b)
+
+number  = 5 
+print (number ==10) #false
+print (number ==5) #true
